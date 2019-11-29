e812f299b4bca3c7e8a24d8c9e36560017f6f16a
= <=>
2019-11-29 15:40:31 -0500

    refactored code and brokeout pieces into seperate functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+//import something from "somewhere"
 
 const test = require("./src/psychopomp");
 const test2 = require("./src/utills");
@@ -8,18 +9,20 @@ const test2 = require("./src/utills");
 const isWin = /^win/.test(process.platform) ? true : false;
 
 // get the list of all files/dirs in the given directory
-// for each file in that list - add file to fileCounts then read File and count imports
+// for each file in that list - add file to importCounts then read File and count imports
 // for each dir in that list - enter dir and list file
-const fileCounts = new Map();
+const importCounts = new Map();
+
 const queue = [["", ""]]; // This is our entrypoint
-
 let promiseList = [];
-
 while (queue.length > 0) {
-  const [name, relativePath] = queue.pop();
-  const currentLocation = path.join(relativePath, name);
+  const [name, currentPath] = queue.pop();
+  const currentLocation = path.join(currentPath, name);
 
+  // What are we working with
+  // I think there is a better way to do this :think:
   const item = fs.statSync(path.join(process.cwd(), currentLocation));
+
   if (item.isDirectory()) {
     // If item is a directory push the new files to the queue
     const fileList = fs.readdirSync(currentLocation);
@@ -28,54 +31,39 @@ while (queue.length > 0) {
       if (item.slice(0, 1) !== ".") queue.push([item, currentLocation]);
     });
   } else if (item.isFile()) {
-    const { length } = name;
     // if we have a js file
-    if (name.slice(length - 3, length) === ".js") {
-      if (!fileCounts.has(currentLocation)) fileCounts.set(currentLocation, 0);
-      // how do I get the logs to print
-      promiseList.push(getImportsFromFile(name, relativePath));
+    if (name.match(/\.js$/)) {
+      // I dont know that we need to do this but will decide later
+      if (!importCounts.has(currentLocation))
+        importCounts.set(currentLocation, 0);
+
+      //
+      promiseList.push(getImportsFromFile(name, currentPath));
     }
   }
 }
 
 printFiles();
 
-async function getImportsFromFile(name, relativePath) {
-  return new Promise((resolve, reject) => {
-    const currentLocation = path.join(relativePath, name);
+/**
+ *
+ *  Defining Functions down here for now
+ *  TODO: Everything down here should go into modules eventually
+ *
+ */
 
+async function getImportsFromFile(name, currentPath) {
+  // I wonder what would be the way to do this in Async/Await syntax?
+  return new Promise((resolve, reject) => {
+    const currentLocation = path.join(currentPath, name);
+
+    // TODO: research more what this is doing
     const readInterface = readline.createInterface({
       input: fs.createReadStream(currentLocation),
       console: false
     });
 
-    readInterface.on("line", line => {
-      const match = line.match(/require\("([\w./]+)"\)/);
-      if (match) {
-        // resolve import path to path from root
-        let importPath;
-        if (relativePath === ".") {
-          importPath = match[1].replace("./", "");
-        } else {
-          importPath = match[1].replace(".", relativePath);
-        }
-        // adjust importPath for OS
-        adjustedPath = isWin ? importPath.replace("/", "\\") : importPath;
-        // add filetype
-        let { length } = adjustedPath;
-        finalPath =
-          adjustedPath.slice(length - 3, length) === ".js"
-            ? adjustedPath
-            : `${adjustedPath}.js`;
-
-        // now the path should match the filepath
-        if (fileCounts.has(finalPath)) {
-          fileCounts.set(finalPath, fileCounts.get(finalPath) + 1);
-        } else {
-          fileCounts.set(finalPath, 1);
-        }
-      }
-    });
+    readInterface.on("line", line => parseImport(line, currentPath));
 
     readInterface.on("close", () => {
       resolve(true);
@@ -83,8 +71,67 @@ async function getImportsFromFile(name, relativePath) {
   });
 }
 
+function parseImport(line, currentPath) {
+  const importPath = getImportPath(line);
+  if (importPath) {
+    // resolve import path to path from root
+    let filePath = getFilePath(importPath, currentPath);
+    // now the path should match the filepath
+    if (importCounts.has(filePath)) {
+      importCounts.set(filePath, importCounts.get(filePath) + 1);
+    } else {
+      importCounts.set(filePath, 1);
+    }
+  }
+}
+
+/**
+ * Uses Regex patterns to find import statements in a line of code
+ * then returns the path used for the import
+ *
+ * ex. line `const myModule = require("./../myAwesomeModule")
+ * would return "./../myAwesomeModule"
+ *
+ * returns null if no match found
+ * @param {*} line
+ */
+function getImportPath(line) {
+  // import with require or import
+  let match =
+    line.match(/require\(["']([\w./]+)["']\)/) ||
+    line.match(/import [\w\d{},]+ from ['"]([\w./]+)['"];?/);
+  return match ? match[1] : null;
+}
+
+/**
+ * takes in the string used in an import statement and uses
+ * that to determine the absolute path to a file location
+ *
+ * Needs to be built out more
+ * @param {*} importName
+ * @param {*} currentPath
+ */
+function getFilePath(importName, currentPath) {
+  let importPath;
+  if (currentPath === ".") {
+    importPath = importName.replace("./", "");
+  } else {
+    importPath = importName.replace(".", currentPath);
+  }
+  // adjust importPath for OS
+  adjustedPath = isWin ? importPath.replace("/", "\\") : importPath;
+  // add filetype
+  let { length } = adjustedPath;
+  const fileName =
+    adjustedPath.slice(length - 3, length) === ".js"
+      ? adjustedPath
+      : `${adjustedPath}.js`;
+
+  return fileName;
+}
+
 async function printFiles() {
   Promise.all(promiseList).then(() => {
-    console.log(fileCounts);
+    console.log(importCounts);
   });
 }
