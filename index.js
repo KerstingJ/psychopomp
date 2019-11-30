fb657643d5fe2f7fd972d0d10c5a4b3768588c87
= <=>
2019-11-30 17:05:45 -0500

    reafactored to use a Deque, and refactoring for a different approach. build list then navigate from entry point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+const Deque = require("collections/deque");
 //import something from "somewhere"
 
 const test = require("./src/psychopomp");
@@ -9,41 +10,42 @@ const test2 = require("./src/utills");
 const isWin = /^win/.test(process.platform) ? true : false;
 
 // get the list of all files/dirs in the given directory
-// for each file in that list - add file to importCounts then read File and count imports
+// for each file in that list - add file to imports then read File and count imports
 // for each dir in that list - enter dir and list file
-const importCounts = new Map();
 
-const queue = [["", ""]]; // This is our entrypoint
-let promiseList = [];
-while (queue.length > 0) {
-  const [name, currentPath] = queue.pop();
-  const currentLocation = path.join(currentPath, name);
+/**
+ * generateFileList navigates our project structure and
+ */
+function generateFileList() {
+  const imports = new Set();
+  const queue = new Deque([["", ""]]); // This is our entrypoint
+  while (queue.length > 0) {
+    const [name, currentPath] = queue.shift();
+    const currentLocation = path.join(currentPath, name);
 
-  // What are we working with
-  // I think there is a better way to do this :think:
-  const item = fs.statSync(path.join(process.cwd(), currentLocation));
+    // What are we working with
+    // I think there is a better way to do this :think:
+    const item = fs.statSync(path.join(process.cwd(), currentLocation));
 
-  if (item.isDirectory()) {
-    // If item is a directory push the new files to the queue
-    const fileList = fs.readdirSync(currentLocation);
-    fileList.forEach(item => {
-      // for now ignore hidden dirs like .git
-      if (item.slice(0, 1) !== ".") queue.push([item, currentLocation]);
-    });
-  } else if (item.isFile()) {
-    // if we have a js file
-    if (name.match(/\.js$/)) {
-      // I dont know that we need to do this but will decide later
-      if (!importCounts.has(currentLocation))
-        importCounts.set(currentLocation, 0);
+    if (item.isDirectory()) {
+      // Ignore node_modules
+      if (currentLocation.match(/node_modules/)) continue;
 
-      //
-      promiseList.push(getImportsFromFile(name, currentPath));
+      const fileList = fs.readdirSync(currentLocation);
+      fileList.forEach(item => {
+        // for now ignore hidden dirs like .git
+        if (item.slice(0, 1) !== ".") queue.push([item, currentLocation]);
+      });
+    } else if (item.isFile()) {
+      // if we have a js file
+      if ([".js"].includes(path.extname(name))) {
+        imports.add(currentLocation);
+      }
     }
   }
-}
 
-printFiles();
+  return imports;
+}
 
 /**
  *
@@ -56,6 +58,7 @@ async function getImportsFromFile(name, currentPath) {
   // I wonder what would be the way to do this in Async/Await syntax?
   return new Promise((resolve, reject) => {
     const currentLocation = path.join(currentPath, name);
+    let imports = [];
 
     // TODO: research more what this is doing
     const readInterface = readline.createInterface({
@@ -63,26 +66,18 @@ async function getImportsFromFile(name, currentPath) {
       console: false
     });
 
-    readInterface.on("line", line => parseImport(line, currentPath));
-
-    readInterface.on("close", () => {
-      resolve(true);
+    readInterface.on("line", line => {
+      const _import = importFromLine(line);
+      if (_import) imports.push(_import);
     });
+
+    readInterface.on("close", () => resolve(imports));
   });
 }
 
-function parseImport(line, currentPath) {
+function importFromLine(line, currentPath) {
   const importPath = getImportPath(line);
-  if (importPath) {
-    // resolve import path to path from root
-    let filePath = getFilePath(importPath, currentPath);
-    // now the path should match the filepath
-    if (importCounts.has(filePath)) {
-      importCounts.set(filePath, importCounts.get(filePath) + 1);
-    } else {
-      importCounts.set(filePath, 1);
-    }
-  }
+  return importPath && getFilePath(importPath, currentPath);
 }
 
 /**
@@ -98,8 +93,8 @@ function parseImport(line, currentPath) {
 function getImportPath(line) {
   // import with require or import
   let match =
-    line.match(/require\(["']([\w./]+)["']\)/) ||
-    line.match(/import [\w\d{},]+ from ['"]([\w./]+)['"];?/);
+    line.match(/require\(["']([\w\./@-]+)["']\)/) ||
+    line.match(/import [\w\d{},]+ from ['"]([\w\./@-]+)['"];?/);
   return match ? match[1] : null;
 }
 
@@ -112,26 +107,20 @@ function getImportPath(line) {
  * @param {*} currentPath
  */
 function getFilePath(importName, currentPath) {
-  let importPath;
-  if (currentPath === ".") {
-    importPath = importName.replace("./", "");
-  } else {
-    importPath = importName.replace(".", currentPath);
+  let importPath = path.join(currentPath, path.normalize(importName));
+  return path.extname(importPath) ? importPath : `${importPath}.js`;
+}
+
+function navigateImports(imports, entry = "index.js") {
+  console.log(imports);
+
+  const queue = new Deque([[entry, ""]]);
+
+  while (queue.length > 0) {
+    const [currentFile, currentPath] = queue.shift();
+
+    // Get a list of the
   }
-  // adjust importPath for OS
-  adjustedPath = isWin ? importPath.replace("/", "\\") : importPath;
-  // add filetype
-  let { length } = adjustedPath;
-  const fileName =
-    adjustedPath.slice(length - 3, length) === ".js"
-      ? adjustedPath
-      : `${adjustedPath}.js`;
-
-  return fileName;
 }
 
-async function printFiles() {
-  Promise.all(promiseList).then(() => {
-    console.log(importCounts);
-  });
-}
+navigateImports(generateFileList());
